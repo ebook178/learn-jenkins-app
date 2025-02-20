5fff74e858ebbd4c59475f54535e3c741adefe92
ebook178 <140891494+ebook178@users.noreply.github.com>
2025-02-20 09:13:40 +0000

    Update App_20Feb-1713

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: {process.env.REACT_APP_VERSI}
+          Application version: {process.env.REACT_APP_VERSION}
       </p>
     </div>
   );
